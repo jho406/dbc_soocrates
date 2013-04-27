7a84003a5081efc5f264b04c8da97b51a207bf7a
Apprentice <apprentice@devbootcamp.com>
2013-04-27 10:58:24 -0700

    generated initial user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :email
+
+  attr_accessible :email, :password, :password_confirmation
+  validates :email, :password, :password_confirmation, :presence=>true
+
   has_secure_password
 end
