b64f02643db3fa67fbdae2a28fbd37ce4f5ef998
Apprentice <apprentice@devbootcamp.com>
2013-04-27 16:01:27 -0700

    added missing duedate column

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130427224137) do
+ActiveRecord::Schema.define(:version => 20130427225935) do
 
   create_table "challenge_units", :force => true do |t|
     t.integer  "challenge_id", :null => false
     t.integer  "unit_id",      :null => false
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.date     "due_date"
   end
 
   create_table "challenges", :force => true do |t|
