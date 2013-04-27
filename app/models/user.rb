45203b8dad31adafea99a94475b5655f147981f6
Apprentice <apprentice@devbootcamp.com>
2013-04-27 10:35:52 -0700

    initial commit- with basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+  attr_accessible :email
+  has_secure_password
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soocrates::Application.config.secret_token = '38800ca92985de62ee53fc1dfc48a460a13ee87e7da7ddbe056d68bcd33e14fc16da5c0c7938a6af8f1882bbf79c73751da924bc6a7ac8f62a7db08a49c93afe'
