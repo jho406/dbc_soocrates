ca79359c2cfb65d0a9aac1d3283df9519c313329
Apprentice <apprentice@devbootcamp.com>
2013-04-27 13:47:19 -0700

    fixed a routing issue and added question functionality

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+  
+  def create
+    question = current_user.questions.new(params[:question])
+    if question.save
+      redirect_to questions_path
+    else
+      render :index
+    end
+  end
+
+  def resolve
+    current_user.questions.find(params[:id]).resolve!
+    redirect_to :back
+  end
+end
