766ad3caf54a9950c2e22f53d663eee011c40924
Apprentice <apprentice@devbootcamp.com>
2013-04-27 23:04:24 -0700

    starting tests for challenges ajax

--- spec/controllers/challenges_controller_spec.rb
+++ spec/controllers/challenges_controller_spec.rb
@@ -3,13 +3,20 @@ require 'spec_helper'
 describe ChallengesController do
   render_views
 
-  let(:cohort){Cohort.create(:name=>"test")}
-  let(:user){User.create(:email=>"j@j.com", :password=>'123', :password_confirmation=>'123')}
-  let(:challenges){}
-  
-  before do
-    session[:user_id] = user.id
+  # let(:cohort){Cohort.create(:name=>"test")}
+  # let(:user){User.create(:email=>"j@j.com", :password=>'123', :password_confirmation=>'123')}
+  # let(:challenges){}
+
+  before(:all) do 
+    load "#{Rails.root}/db/seeds.rb"  
   end
-  it 'should list challenges'
+
+  before do
+    session[:user_id] = User.first.id
+  end
+
+  it 'should list challenges' do 
+    1.should eql(1)
+  end
   it 'should show a challenge'
 end
